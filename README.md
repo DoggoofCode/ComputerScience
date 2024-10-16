8711d0df462ddaefefaf627a6473b9bbc7c93f9b
vedjaggi <vedajaggi@gmail.com>
2024-10-16 21:00:27 +0800

    Creates code Structure, and Sidebar

---
+++ README.md
@@ -0,0 +1,8 @@
+# Trajectory Calculator
+To run this program, firstly install all the required modules with:
+> pip install requirements.txt
+
+Then, afterward, run:
+> streamlit run main.py
+
+This should open the website
--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 import streamlit as st
+import numpy as np
+
+class TitlePage:
+    @classmethod
+    def run(cls):
+        pass
+
+class SingleSolver:
+    @classmethod
+    def run(cls):
+        pass
+
+class GeneralSolver:
+    @classmethod
+    def run(cls):
+        pass
 
 def main():
-    st.title("Trajectory!")
+    st.set_page_config(
+        layout="wide",
+        page_title="Trajectory Solver"
+    )
+
+    solver_mode = st.sidebar.selectbox(
+        "",
+        [
+            "Title Screen",
+            "General Solve",
+            "Single Solve",
+        ]
+    )
+
+    if solver_mode == "Title Screen":
+        TitlePage.run()
+    elif solver_mode == "General Solve":
+        GeneralSolver.run()
+    elif solver_mode == "Single Solve":
+        SingleSolver.run()
 
 if __name__ == '__main__':
     main()
